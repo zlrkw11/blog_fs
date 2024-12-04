a7d501e30b4162ba4b24731092ca97b25bb106b7
ZL <ray040406@gmail.com>
2024-12-04 14:12:01 +1300

    server set up

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,7 @@
 // const express = require("express");
 import express from "express";
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server listening at http://localhose:5000");
+});
