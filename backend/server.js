9212cdf40fd03ee133580d8cdc06515f7955024c
ZL <ray040406@gmail.com>
2024-12-04 14:20:05 +1300

    get

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 import express from "express";
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
 app.listen(5000, () => {
-  console.log("Server listening at http://localhose:5000");
+  console.log("Server listening at http://localhost:5000");
 });
